e8ce2d42c9781bcb17fd3804b604c14dc976dc54
razan kurouni <razankr07@gmail.com>
2024-09-19 09:28:34 +0300

    initial commit

---
+++ src/components/slider2/dataSlides.jsx
@@ -0,0 +1,33 @@
+// sliderData.js
+import image1 from './../../assets/img/hero-carousel/hero-carousel-1.jpg';
+import image2 from './../../assets/img/hero-carousel/hero-carousel-2.jpg';
+import image3 from './../../assets/img/hero-carousel/hero-carousel-3.jpg';
+
+const dataSlides = [
+    {
+        image: image1,
+        title: 'Welcome to Medicio',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat.`
+    },
+    {
+        image: image2,
+        title: 'Welcome to Medicio',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat.`
+    },
+    {
+        image: image3,
+        title: 'Welcome to Medicio',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat.`
+    }
+];
+
+export default dataSlides;
